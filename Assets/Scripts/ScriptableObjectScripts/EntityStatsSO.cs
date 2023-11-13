68e9d1ce7ecaec9bc2087d02c2923fce3df9b506
Woopsious <reecehand@hotmail.co.uk>
2023-11-13 23:04:47 +0000

    update

--- Assets/Scripts/ScriptableObjectScripts/EntityStatsSO.cs
+++ Assets/Scripts/ScriptableObjectScripts/EntityStatsSO.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "EntityStatsScriptableObject", menuName = "EntityStats")]
 public class BaseStatsSO : ScriptableObject
 {
+	public int health;
 
+	public int physicalDamageResistance;
+	public int poisonDamageResistance;
+	public int fireDamageResistance;
+	public int iceDamageResistance;
+
+	public int moveSpeed;
+	public int mana;
 }
