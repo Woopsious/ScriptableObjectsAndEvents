06afa1c40b5210f1cc152ead9b34642a85a566f8
Woopsious <reecehand@hotmail.co.uk>
2023-11-30 20:29:19 +0000

    created simple idle behaviour for enemies.

--- Assets/Scripts/Entities/EntityBehaviour.cs
+++ Assets/Scripts/Entities/EntityBehaviour.cs
@@ -3,33 +3,90 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Threading;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EntityBehaviour : MonoBehaviour
 {
-	private NavMeshAgent navMeshAgent;
-	private Rigidbody rb;
+	public Bounds idleBounds;
+	public Vector3 movePosition;
+	public bool HasReachedDestination;
+
+	public NavMeshAgent navMeshAgent;
+	public Rigidbody rb;
 
 	public float timer;
 	public float timerCooldown = 10f;
 
 	public void Start()
 	{
-		rb = GetComponent<Rigidbody>();
-		navMeshAgent = GetComponent<NavMeshAgent>();
+		idleBounds.min = new Vector3(transform.position.x - 50, transform.position.y - 3, transform.position.z - 50);
+		idleBounds.max = new Vector3(transform.position.x + 50, transform.position.y + 3, transform.position.z + 50);
+
+		HasReachedDestination = true;
 	}
 	public void Update()
 	{
-		timer -= Time.deltaTime;
-
-		if (timer < 0)
+		IdleAtPosition();
+		CheckDistance();
+	}
+	public void IdleAtPosition()
+	{
+		if (HasReachedDestination == true)
 		{
-			timer = timerCooldown;
+			timer -= Time.deltaTime;
 
-			//Debug.LogWarning(equippedWeapon.damage);
-			//Debug.LogWarning(equippedWeapon.itemLevel);
-			//Debug.LogWarning(equippedWeapon.rarity);
+			if (timer < 0)
+			{
+				timer = timerCooldown;
+				FindNewIdlePosition();
+			}
 		}
+	}
+	public void FindNewIdlePosition()
+	{
+		SampleNewMovePosition();
+
+		Debug.LogWarning("position found");
+		HasReachedDestination = false;
+	}
+	public bool SampleNewMovePosition()
+	{
+		Vector3 randomMovePosition = Utilities.GetRandomPointInBounds(idleBounds);
+
+		NavMesh.SamplePosition(randomMovePosition, out NavMeshHit navMeshHit, 25, navMeshAgent.areaMask);
+		movePosition = navMeshHit.position;
+
+		if (CheckAndSetNewPath(movePosition))
+			return true;
+		else return false;
+	}
+	public bool CheckAndSetNewPath(Vector3 movePosition)
+	{
+		NavMeshPath path = new NavMeshPath();
+		if (navMeshAgent.CalculatePath(movePosition, path))
+		{
+			navMeshAgent.SetPath(path);
+			navMeshAgent.isStopped = false;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public void CheckDistance()
+	{
+		if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance && HasReachedDestination == false)
+		{
+			HasReachedDestination = true;
+			navMeshAgent.isStopped = true;
+		}
+	}
+	public void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawCube(idleBounds.center, idleBounds.size);
 	}
 }
--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -1,10 +1,18 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Utilities
 {
 	public static int GetRandomNumber(int num)
 	{
 		return Random.Range(0, num);
+	}
+	public static Vector3 GetRandomPointInBounds(Bounds bounds)
+	{
+		return new Vector3(
+			Random.Range(bounds.min.x, bounds.max.x),
+			Random.Range(bounds.min.y, bounds.max.y),
+			Random.Range(bounds.min.z, bounds.max.z));
 	}
 }
